74e78689e64971bf7cc51a29a0be7e314879ee22
Cristian Felipe Jiménez Varón <117649994+cfjimenezv07@users.noreply.github.com>
2024-02-04 14:09:03 +0300

    Update HNT23_GSY19.R

--- Rcodes_paper/HNT23_GSY19.R
+++ Rcodes_paper/HNT23_GSY19.R
@@ -1,4 +1,4 @@
-# Replicate Hallin et al. 2023b (HNT) and Gao et al. 2019 (GSY) methods for our datasets. 
+# Replicate Hallin et al. 2023 (HNT) and Gao et al. 2019 (GSY) methods for our datasets. 
 # These Rcodes are public posted in https://zenodo.org/record/7408999 from HNT23, it includes GSY19.
 # We adapted to our paper cases.
 
